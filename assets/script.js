b1e91931ef3ba5a61853978fd02af09172fd7e5a
luis <florezf90@gmail.com>
2023-09-18 18:06:27 -0500

    changed the scores array value in order to prevent to reset every time a user goes trough the quiz

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // getting a handle of the elements
-
+var input = document.querySelector(".USERPROMPT");
 var start = document.querySelector("#start");
 var board = document.querySelector("#Scores");
 var aa = document.querySelector("#aa");
@@ -26,42 +26,63 @@ var setquestions = [" question 1", "question 2", "question 3", "question 4", "qu
       var answersd = ["question 1", "question 2","question 3","question 4","question 5","question 6","question 7","question 8",
       "question 9","question 10", ];
 
-
+var scores = JSON.parse(localStorage.getItem("player: ")) || [];
 var questionumber = 1;
 var currentscore = 0;
+var stopTimer = false;
 
 // this is the code that is gonna start running when the user clicks on the "let's start button"
-
-
 function test () {
 
       // this code present the first question and creates the buttons for the possible answers
-    
-    
        title.textContent = setquestions[0]
          presentation.textContent = "Keep in mind buddy.....Each time you fail a question, the time left will be penalized.....     ";
 
          var optionA = document.createElement("button");
-             optionA.textContent = "option A";
+         optionA.classList.add("optionA");
+             optionA.textContent = answersA[0];
              questions.appendChild(optionA);  
          
              var optionB = document.createElement("button");
-             optionB.textContent = "option B";
+             optionB.classList.add("optionB");
+             optionB.textContent = answersb[0];
              questions.appendChild(optionB);  
          
              var optionC = document.createElement("button");
-             optionC.textContent = "option C";
+             optionC.classList.add("optionC");
+             optionC.textContent = answersc[0];
              questions.appendChild(optionC);  
 
              var optionD = document.createElement("button");
-             optionD.textContent = "option D";
+             optionD.classList.add("optionD");
+             optionD.textContent = answersd[0];
              questions.appendChild(optionD);  
 
              var tracker = document.createElement("p");
              questions.appendChild(tracker);  
 
-             var input = document.createElement("input");
-             questions.appendChild(input);
+                          // following code creates the timer
+                          var timer = document.createElement("h3");
+                          body.appendChild(timer);
+                          var timeleft = 60;
+             
+                          function seTime() {
+                             var timeinterval = setInterval(() => {
+                                if (!stopTimer) {
+                                 timeleft--;
+                                 timer.textContent= timeleft + " secs till your are done!!!!";
+             
+                                 if (timeleft === 0 ) {
+                                     clearInterval(timeinterval);
+                                     final();
+                                 }
+                                }
+                             }, 1000);
+                             
+                          }
+             
+                          seTime();
+                          buttons.setAttribute("style", "display: none");
 
             // this code will present the total score of the user, remove the button elements created before and will present the form list 
             // to the user so they can save their scores
@@ -77,71 +98,105 @@ function test () {
                 input.setAttribute("style", "display: inline;" );
                 aa.setAttribute("style", "display: inline;" );
                 presentation.textContent = "Please provide your name to be added to the scoreboard ";
-                            
+                timer.textContent= " Thank you for participating";
+
+                var submision = document.createElement("ol");
+                    submision.classList.add("leaderboard");
+                    submision.textContent = "Top-Scores"
+                    tracker.appendChild(submision);
+
+                    function save() {
+                        localStorage.setItem("player: ", JSON.stringify(scores));
+                    }
+
+                    function render() {
+                        var lastry = JSON.parse(localStorage.getItem("player: "));
+                        if (lastry !== null ) {
+                            var submision = document.querySelector(".leaderboard");
+                            submision.innerHTML = "";
+                            for (var i = 0; i < lastry.length; i++) {
+                                var attempt = document.createElement("li");
+                                attempt.textContent = lastry[i];
+                                submision.appendChild(attempt);
+                              }
+                        }
+                    }
+
+                 render();
+                 
+                aa.addEventListener("click", function  (event) {
+
+                    event.preventDefault();
+                    var playerScore = input.value + " ( " + currentscore + " )";
+                    scores.push(playerScore);
+                    console.log(scores);
+                    save();
+                    render();
+
+
+                })
             }
 
             // The following array, function & for loop of code will present to the user
             // a new question & answers every time they click on a button
              var Answers = [optionA, optionB, optionC, optionD];
-             var correctanswer = ["question 1", "question 2", "question 2", "question 4","question 1", "question 2", "question 2", "question 1", "question 2", "question 2"];
-             var buttonsclicked = [];
+             var correctanswer = [
+                "optionA", // Correct answer for question 1
+                "optionB", // Correct answer for question 2
+                "optionC", // Correct answer for question 3
+                "optionD", // Correct answer for question 4
+                "optionA", // Correct answer for question 5
+                "optionB", // Correct answer for question 6
+                "optionC", // Correct answer for question 7
+                "optionD", // Correct answer for question 8
+                "optionA", // Correct answer for question 9
+                "optionB"  // Correct answer for question 10
+              ];
 
 
              function change() {
-                if (questionumber < setquestions.length || questionumber < answersA.length || questionumber < answersb.length  || 
-                    questionumber < answersc.length || questionumber < answersd.length || questionumber < correctanswer.lenght) {
+                if (questionumber < setquestions.length) {
                     optionA.textContent = answersA[questionumber];
                     optionB.textContent = answersb[questionumber];
                     optionC.textContent = answersc[questionumber];
                     optionD.textContent = answersd[questionumber];
                     title.textContent = setquestions[questionumber];
                     questionumber++;
-                } else if (questionumber = 10 ){
+                } else if (questionumber = 9 ){
                 final();
                 } 
             }
 
                 
              for (let i = 0; i < Answers.length; i++) {
-                Answers[i].addEventListener("click", change)
+                // Answers[i].addEventListener("click", change)
                 Answers[i].addEventListener("click", function inputracker (event){
-                    buttonsclicked.push(event.target.textContent);
-                    if (buttonsclicked.textContent === correctanswer.textContent) {
-                        console.log("working");
-                        console.log(buttonsclicked);
+                    // console.log(event.currentTarget.className);
+                    if (event.currentTarget.className == correctanswer[questionumber - 1]) {
+                        console.log(correctanswer[i]);
+                        // console.log(optionA.className);
                          currentscore = currentscore + 1;
                          tracker.textContent = "your current score is " + currentscore;
 
+                        } else {
+                                timeleft = timeleft - 10; 
                         }
+                        
+                        if (questionumber < setquestions.length) {
+                            change();
+                        } else {
+                            // If all questions are answered, stop the timer and show the final results
+                            stopTimer = true;
+                            final();
+                          }
+                        if (timeleft <= 0 ) {
+                            stopTimer = true;
+                            final();
 
-                    
-                })
+                          }
+                });
                 
              }
-
-             // following code creates the timer
-             
-
-             var timer = document.createElement("h3");
-             body.appendChild(timer);
-             var timeleft = 10;
-
-             function seTime() {
-                var timeinterval = setInterval(() => {
-                    timeleft--;
-                    timer.textContent= timeleft + " secs till your are done!!!!";
-
-                    if (timeleft === 0 ) {
-                        clearInterval(timeinterval);
-                        final();
-                    }
-                }, 1000);
-                
-             }
-
-             seTime();
-             buttons.setAttribute("style", "display: none");
-
 }
 
 // this code will deploy and present the leaderboard to the user
@@ -153,7 +208,10 @@ function scoreboard() {
 
 
 // this are the event listeners, depending of which button the user clicks, a different block of code will deploy
-start.addEventListener("click", test);
+start.addEventListener("click", function () {
+    userScore = 0; 
+    test(); 
+  });
 board.addEventListener("click", scoreboard);
 
 
